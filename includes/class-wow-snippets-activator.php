eb19a9e87afd2909b6b56124dd906e21d2cac0ab
Md Yakub Mizan <md.yakub.mizan@gmail.com>
2025-03-12 01:35:07 +0600

    Imported plugin boilterplate

---
+++ includes/class-wow-snippets-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wowwp.com
+ * @since      1.0.0
+ *
+ * @package    Wow_Snippets
+ * @subpackage Wow_Snippets/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wow_Snippets
+ * @subpackage Wow_Snippets/includes
+ * @author     WOW WP <help@wowwp.com>
+ */
+class Wow_Snippets_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/partials/wow-snippets-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://wowwp.com
+ * @since      1.0.0
+ *
+ * @package    Wow_Snippets
+ * @subpackage Wow_Snippets/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
